8acf6a753aad5e0d4b2bbac16b269007ff4c138b
12gina <cometome1212@gmail.com>
2025-08-07 06:26:54 +0900

    update 반응형 적용
    update 반응형 적용

--- src/components/sectionLayout.jsx
+++ src/components/sectionLayout.jsx
@@ -3,8 +3,8 @@ export const SectionLayout = ({ref, id, title, children}) => {
     return (
         <section className={'section-layout'} ref={ref} id={id}>
           <div>
+            <div>{title}</div>
             <div>
-              <div>{title}</div>
               {children}
             </div>
           </div>

39b16d553cef5dba363e73ca869b610e0de6c113
12gina <cometome1212@gmail.com>
2025-08-11 05:19:35 +0900

    update 스타일 인라인으로 수정중
    update 스타일 인라인으로 수정중

--- src/App.jsx
+++ src/App.jsx
@@ -16,146 +16,175 @@ gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   
-    const [isMenuModalOpen, setIsMenuModalOpen] = useState(false);
-    const [isMenuButtonOn, setIsMenuButtonOn] = useState(false);
-    const [currentSection, setCurrentSection] = useState("aboutMe");
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [isMobile, setIsMobile] = useState(false);
+  const [isMenuModalOpen, setIsMenuModalOpen] = useState(false);
+  const [isMenuButtonOn, setIsMenuButtonOn] = useState(false);
+  const [currentSection, setCurrentSection] = useState("aboutMe");
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isPlayed, setIsPlayed] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-    const modalOpenTimerRef = useRef(null);
-    const navOnTimerRef = useRef(null);
-    const sectionRefs = useRef({
-      aboutMe: null,
-      experiences: null,
-      projects: null,
-      skills: null,
+  const modalOpenTimerRef = useRef(null);
+  const navOnTimerRef = useRef(null);
+  const sectionRefs = useRef({
+    title: null,
+    aboutMe: null,
+    experiences: null,
+    projects: null,
+    skills: null,
+  });
+
+  useGSAP(()=>{
+
+    if (!isLoaded) return;
+    
+    let tl = gsap.timeline({paused: true});
+
+    title2.forEach((_, i)=>{
+      tl.to(
+        `#char2_${i}`,
+        { duration: 0.5, y: 0, scale: 1, opacity: 1, ease: "power2.out" },
+          i === 0 ? undefined : "<0.1")
+    })
+    title1.forEach((_, i)=>{
+      tl.to(
+        `#char1_${i}`,
+        { duration: 0.5, y: 0, scale: 1, opacity: 0.6, ease: "power2.out" },
+          i === 0 ? "<0.1" : "<0.1")
+    })
+    
+    tl.to('#hr1', {duration: 0.5, x: 0, ease: "power2.out" },"<0.2")
+    .to('#hr2', {duration: 0.5, x: 0, ease: "power2.out" }, "<")
+    .to('.menu-button', {duration: 0.3, opacity: 1, ease: "power2.out" }, ">0.1")
+
+    const st = ScrollTrigger.create({
+      animation: tl,
+      trigger: "#title-wrap",
+      start: "top top",
+      end: "top top",
+      pin: true,
+      scrub: false,
+      once: true,
+      anticipatePin: 1,
+      invalidateOnRefresh: true,
+    },);
+
+    tl.eventCallback("onComplete", () => {
+      // st.kill();
+      setIsPlayed(true);    
     });
 
-    useGSAP(()=>{
+  },[{dependencies: [isLoaded]}])
 
-      if (!isLoaded) return;
+  useGSAP(()=>{
+
+    if (!isPlayed) return;
+  
+    let tl2 = gsap.timeline({paused: true});
+
+    title1.forEach((_, i)=>{
+      tl2.to(
+        `#char1_${title1.length -1- i}`,
+        { duration: 0.5, y: "70%", scale: 5, opacity: 0, ease: "power2.out" },
+          i === 0 ? undefined : "<0.1")
+    })
+    title2.forEach((_, i)=>{
+      tl2.to(
+        `#char2_${title2.length -1 - i}`,
+        { duration: 0.5, y: "-70%", scale: 5, opacity: 0, ease: "power2.out" },
+          i === 0 ? "<0.1" : "<0.1")
+    })
+    
+    tl2.to('#hr1', {duration: 0.5, x: '-120%', ease: "power2.out" },"<0.2")
+    .to('#hr2', {duration: 0.5, x: '120%', ease: "power2.out" }, "<")
+
+    ScrollTrigger.create({
+      animation: tl2,
+      trigger: "#title",
+      start: "top top",
+      end: "+=80%",
+      pin: true,
+      scrub: 1,
+      toggleActions: "none play reverse play",
+      anticipatePin: 1,
+      invalidateOnRefresh: true,
+      markers: false,
+    },);
+
+
+  },[{dependencies: [isPlayed]}])
+  // },[])
+
+    // useGSAP(()=>{
+
+    //   // let tl2 = gsap.timeline();
+    //   // tl2.to("#photo", {duration: 0.5,  scale: 1, rotate:360, ease: "power2.out"}, "<0.2")
+
+    //   let tl3 = gsap.timeline();
+    //   expData.forEach((_, i)=>{
+    //     tl3.to(
+    //       `#exp-card_${i}`,
+    //       { duration: 0.3, opacity: 1, y: 0, ease: "power2.out" })})
+
+    //   let tl4 = gsap.timeline();
+    //     tl4.to('#project1', {duration: 0.5,  x: 0, ease: "power2.out"})
+    //     .to('#project2', {duration: 0.5,  x: 0, ease: "power2.out" }, "<0.2") 
       
-      let tl = gsap.timeline({
-        paused: true,
-      });
-
-      title1.forEach((_, i)=>{
-        tl.to(
-          `#char1_${i}`,
-          { duration: 0.5, y: 0, scale: 1, opacity: 1, ease: "power2.out" },
-            i === 0 ? undefined : "<0.1")
-      })
-      title2.forEach((_, i)=>{
-        tl.to(
-          `#char2_${i}`,
-          { duration: 0.5, y: 0, scale: 1, opacity: 1, ease: "power2.out" },
-            i === 0 ? "<0.1" : "<0.1")
-      })
+    //   let tl5 = gsap.timeline();
+    //   skillData.forEach((item, idx)=>{
+    //     item.skills.forEach((_, i)=>{
+    //       tl5.to(
+    //               `#skill_${idx}_${i}`,
+    //               { duration: 0.3, opacity: 1, scale: 1.2, ease: "sine.in" },
+    //                 i === 0 ? undefined : "<-0.25"
+    //         ).to(
+    //           `#skill_${idx}_${i}`,
+    //             { duration: 0.2, scale: 1, ease: "sine.in" },
+    //             ">"
+    //       )
+    //     })
+    //   })
       
-      tl.to('#hr1', {duration: 0.5, x: 0, ease: "power2.out" },"<0.2")
-      .to('#hr2', {duration: 0.5, x: 0, ease: "power2.out" }, "<")
-      .to('.menu-button', {duration: 0.3, opacity: 1, ease: "power2.out" }, ">0.1")
+    //   // ScrollTrigger.create({
+    //   //   animation: tl2,
+    //   //   trigger: "#aboutMe",
+    //   //   start: "top top",
+    //   //   end: "+=150%",
+    //   //   toggleActions: 'restart none restart none',
+    //   //   onEnter: () => tl2.play(),
+    //   //   invalidateOnRefresh: true,
+    //   // },);
 
-      ScrollTrigger.create({
-        animation: tl,
-        trigger: "#page",
-        start: "top top",
-        end: "+=100%",
-        pin: true,
-        pinSpacing: true,
-        pinReparent: true,
-        // onEnter: ()=>tl.play(),
-        // toggleActions: 'restart none restart none',
-        // scrub: false,
-        anticipatePin: 1,
-        invalidateOnRefresh: true,
-      },);
+    //   ScrollTrigger.create({
+    //     animation: tl3,
+    //     trigger: "#experiences",
+    //     start: "top center",
+    //     end: "center center",
+    //     scrub: 1,
+    //     toggleActions: 'restart none restart none',
+    //     onEnter: () => tl3.restart(),
+    //     invalidateOnRefresh: true,
+    //     // markers: true
+    //   },);
 
-      ScrollTrigger.create({
-        trigger: "#page",
-        start: "top top",
-        end: "bottom bottom",
-        snap: {
-          snapTo: 1 / 4,      // 구간 비율 (예: 섹션 4개면 1/4)
-          duration: 0.3,      // 스냅 애니메이션 시간(초)
-          delay: 0,           // 스냅 전 대기시간
-          ease: "power1.inOut"
-        }
-      })
+    //   ScrollTrigger.create({
+    //     animation: tl4,
+    //     trigger: "#projects",
+    //     start: "top +=60%",
+    //     toggleActions: 'restart none restart none',
+    //     onEnter: () => tl4.play(),
+    //     invalidateOnRefresh: true,
+    //   },);
 
-    },[isLoaded])
+    //   ScrollTrigger.create({
+    //     animation: tl5,
+    //     trigger: "#skills",
+    //     start: "top center",
+    //     toggleActions: 'restart none restart none',
+    //     onEnter: () => tl5.play(),
+    //     invalidateOnRefresh: true,
+    //   },);
 
-    useGSAP(()=>{
-
-      // let tl2 = gsap.timeline();
-      // tl2.to("#photo", {duration: 0.5,  scale: 1, rotate:360, ease: "power2.out"}, "<0.2")
-
-      let tl3 = gsap.timeline();
-      expData.forEach((_, i)=>{
-        tl3.to(
-          `#exp-card_${i}`,
-          { duration: 0.3, opacity: 1, y: 0, ease: "power2.out" })})
-
-      let tl4 = gsap.timeline();
-        tl4.to('#project1', {duration: 0.5,  x: 0, ease: "power2.out"})
-        .to('#project2', {duration: 0.5,  x: 0, ease: "power2.out" }, "<0.2") 
-      
-      let tl5 = gsap.timeline();
-      skillData.forEach((item, idx)=>{
-        item.skills.forEach((_, i)=>{
-          tl5.to(
-                  `#skill_${idx}_${i}`,
-                  { duration: 0.3, opacity: 1, scale: 1.2, ease: "sine.in" },
-                    i === 0 ? undefined : "<-0.25"
-            ).to(
-              `#skill_${idx}_${i}`,
-                { duration: 0.2, scale: 1, ease: "sine.in" },
-                ">"
-          )
-        })
-      })
-      
-      // ScrollTrigger.create({
-      //   animation: tl2,
-      //   trigger: "#aboutMe",
-      //   start: "top top",
-      //   end: "+=150%",
-      //   toggleActions: 'restart none restart none',
-      //   onEnter: () => tl2.play(),
-      //   invalidateOnRefresh: true,
-      // },);
-
-      ScrollTrigger.create({
-        animation: tl3,
-        trigger: "#experiences",
-        start: "topc center",
-        end: "center center",
-        scrub: 1,
-        toggleActions: 'restart none restart none',
-        onEnter: () => tl3.restart(),
-        invalidateOnRefresh: true,
-        // markers: true
-      },);
-
-      ScrollTrigger.create({
-        animation: tl4,
-        trigger: "#projects",
-        start: "top +=60%",
-        toggleActions: 'restart none restart none',
-        onEnter: () => tl4.play(),
-        invalidateOnRefresh: true,
-      },);
-
-      ScrollTrigger.create({
-        animation: tl5,
-        trigger: "#skills",
-        start: "top center",
-        toggleActions: 'restart none restart none',
-        onEnter: () => tl5.play(),
-        invalidateOnRefresh: true,
-      },);
-
-    },[])
+    // },[])
 
     useEffect(()=>{
         if (isMenuButtonOn) {
@@ -216,35 +245,36 @@ function App() {
 
   return (
     <>
-      <main id={"page"}>
+      <main id={"page"} style={{width: "100vw", height: '100%', color: "white", display: "flex", flexDirection: "column", overflowX: "hidden", position: "relative" }}>
 
+        <video id={'intro-video'} onCanPlayThrough={()=>setIsLoaded(true)} autoPlay muted loop playsInline
+          style={{width: "100%", height: '100vh', position:"fixed", inset:0, color: "white", overflowX: "hidden", objectFit:'cover', }}>
+          <source src={introVideo} type="video/mp4"/>
+        </video>
+        
         {/* 타이틀 */}
-        <section id={"title"}>
+        <div id={'title-wrap'}>
+        <section id={"title"} ref={el => sectionRefs.current.title = el} style={{width: "100%", height: '100vh', display: 'flex', flexDirection: "column", justifyContent: "flex-end", alignItems:"center", zIndex:1, fontSize: 'clamp(0.7vw, 7vw, 70rem)', padding: '10rem 5rem', boxSizing: 'border-box'}}>
 
-          <video id={'intro-video'} onCanPlayThrough={()=>setIsLoaded(true)} autoPlay muted loop playsInline>
-            <source src={introVideo} type="video/mp4"/>
-          </video>
-
-          <div>
-            <div id={"title1"}>
-              {title1.map((v, i)=>(
-                <span key={i} id={`char1_${i}`}>{v}</span>
-              ))}
-            </div>
-
-            <div>
-              <div id={"hr1"}/>
-              <div id={"hr2"}/>
-            </div>
-
-            <div id={"title2"}>
+            <div id={"title2"} style={{width: "100%", display: 'flex', textAlign: "center", columnGap: "2rem", justifyContent: "center", textShadow:"5px 5px 3px rgba(0, 0, 0, 0.5)" }}>
               {title2.map((v, i)=>(
-                <span key={i} id={`char2_${i}`}>{v}</span>
+                <span key={i} id={`char2_${i}`} style={{opacity: 0, scale:5}}>{v}</span>
               ))}
             </div>
-          </div>
+
+            <div style={{width: "100%", height: 'calc(6rem + 2px)', display: 'flex', justifyContent: "space-between" }}>
+              <div id={"hr1"} style={{flex: 1, height: 2, backgroundColor: "white", margin: '3rem 0', transform: "translateX(-120%)"}}/>
+              <div id={"hr2"} style={{flex: 1, height: 2, backgroundColor: "white", margin: '3rem 0', transform: "translateX(120%)"}}/>
+            </div>
+
+            <div id={"title1"} style={{width: "100%", display: 'flex', fontWeight: 900, textAlign: "center", columnGap: "1rem", justifyContent: "center", textShadow: "5px 5px 3px rgba(0, 0, 0, 0.5)" }}>
+              {title1.map((v, i)=>(
+                <span key={i} id={`char1_${i}`} style={{opacity: 0, scale:5}}>{v}</span>
+              ))}
+            </div>
 
         </section>
+        </div>
 
         {/* 자기소개 */}
         <SectionLayout title={"About me"} id={"aboutMe"} ref={el => sectionRefs.current.aboutMe = el}>
@@ -279,14 +309,14 @@ function App() {
         </SectionLayout>
         
         {/* 경력 */}
-        <SectionLayout title={"Experiences"} id={"experiences"} ref={el => sectionRefs.current.experiences = el}>
+        {/* <SectionLayout title={"Experiences"} id={"experiences"} ref={el => sectionRefs.current.experiences = el}>
             {expData.map((v, i)=>(
               <ExpCard key={v.company} company={v.company} department={v.department} duration={v.duraiton} description={v.description} idx={i}/>
             ))}
-        </SectionLayout>
+        </SectionLayout> */}
 
         {/* 프로젝트 */}
-        <SectionLayout title={"Projects"} id={"projects"} ref={el => sectionRefs.current.projects = el}>
+        {/* <SectionLayout title={"Projects"} id={"projects"} ref={el => sectionRefs.current.projects = el}>
           <div id={`project1`}>
             {projectData.slice(0,3).map((v,i)=>(
               <ProjectCard key={v.title} image={v.image} title={v.title} description={v.description} skills={v.skills} task={v.task} device={v.device} href={v.href} idx={i}/>
@@ -297,14 +327,14 @@ function App() {
               <ProjectCard key={v.title} image={v.image} title={v.title} description={v.description} skills={v.skills} task={v.task} device={v.device} href={v.href} idx={i+3}/>
             ))}
           </div>
-        </SectionLayout>
+        </SectionLayout> */}
 
         {/* 스킬 */}
-        <SectionLayout title={"Skills"} id={"skills"} ref={el => sectionRefs.current.skills = el}>
+        {/* <SectionLayout title={"Skills"} id={"skills"} ref={el => sectionRefs.current.skills = el}>
           {skillData.map((v, i)=>(
             <SkillCard key={i} description={v.description} skills={v.skills} idx={i}/>
           ))}
-        </SectionLayout>
+        </SectionLayout> */}
 
       </main>
 
